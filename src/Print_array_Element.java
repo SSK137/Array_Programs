1532d0f3d9489e75c8ac748d7bd8944e347ee1af
SSK137 <saurabhkodam@gmail.com>
2022-05-01 12:58:13 +0530

    Array Program : Print Element

---
+++ src/Print_array_Element.java
@@ -0,0 +1,14 @@
+public class Print_array_Element {
+    public static void main(String[] args) {
+        int []arr = new int[5];
+        arr[0] = 10;
+        arr[1] = 20;
+        arr[2] = 30;
+        arr[3] = 40;
+        arr[4] = 50;
+        System.out.println("Array Elements : ");
+        for (int i=0;i<arr.length;i++) {
+            System.out.println(arr[i]);
+        }
+    }
+}
