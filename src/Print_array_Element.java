a60caee1555982885cba051fcdf81606457b5848
SSK137 <saurabhkodam@gmail.com>
2022-05-01 13:05:13 +0530

    Array Program : Frequency of elements

--- src/Print_array_Element.java
+++ src/Print_array_Element.java
@@ -1,14 +1,31 @@
 public class Print_array_Element {
-    public static void main(String[] args) {
-        int []arr = new int[5];
-        arr[0] = 10;
-        arr[1] = 20;
-        arr[2] = 30;
-        arr[3] = 40;
-        arr[4] = 50;
-        System.out.println("Array Elements : ");
-        for (int i=0;i<arr.length;i++) {
-            System.out.println(arr[i]);
+    public static void main(String[] args)
+    {
+        //Initialization of array
+        int [] arr1 = new int [] {1, 2, 8, 3, 2, 2, 2, 5, 1};
+        //Array arr2 will store frequencies of element
+        int [] arr2 = new int [arr1.length];
+        int visited = -1;
+        for(int i = 0; i < arr2.length; i++){
+            int count = 1;
+            for(int j = i+1; j < arr1.length; j++){
+                if(arr1[i] == arr1[j]){
+                    count++;
+                    //To avoid counting same element again
+                    arr2[j] = visited;
+                }
+            }
+            if(arr2[i] != visited)
+                arr2[i] = count;
+        }
+
+        //Displays the frequency of each element present in array
+        ;
+        System.out.println(" Element | Frequency");
+
+        for(int i = 0; i < arr1.length; i++){
+            if(arr2[i] != visited)
+                System.out.println("    " + arr1[i] + "       " + arr2[i]);
         }
     }
 }
