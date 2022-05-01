6e871372b0649877123845d1ad501748f05529e1
SSK137 <saurabhkodam@gmail.com>
2022-05-01 13:28:04 +0530

    Array Program : even position element

---
+++ src/Element_Even_Position.java
@@ -0,0 +1,15 @@
+public class Element_Even_Position {
+    public static void main(String[] args) {
+        //Initializing an array
+        int []arr = new int[5];
+        arr[0] = 10;
+        arr[1] = 20;
+        arr[2] = 30;
+        arr[3] = 40;
+        arr[4] = 50;
+        //loop for even positions element using i=i+2
+        for (int i=0;i<arr.length;i=i+2) {
+            System.out.println("Array elements are " + arr[i]);
+        }
+    }
+}
